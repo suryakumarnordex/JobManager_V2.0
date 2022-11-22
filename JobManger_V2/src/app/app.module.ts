7ed4d8d8b4ec08ef8b787e3b2245d7d0987913c1
SrivigneshUmasankaran <UmasankarS@NORDEX-AG.COM>
2022-11-22 14:46:50 +0530

    npm installation completed

--- JobManger_V2/src/app/app.module.ts
+++ JobManger_V2/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClarityModule } from "@clr/angular";
+import { AppComponent } from "./app.component";
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        ClarityModule,
+        AppRoutingModule,
+        
+     ],
+     declarations: [ AppComponent ],
+     bootstrap: [ AppComponent ]
 })
 export class AppModule { }
