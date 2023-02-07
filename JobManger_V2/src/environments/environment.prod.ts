de90195db622be28f323f57f9c290c83b783b100
SrivigneshUmasankaran <UmasankarS@NORDEX-AG.COM>
2023-02-07 13:56:42 +0530

    added userlogin

--- JobManger_V2/src/environments/environment.prod.ts
+++ JobManger_V2/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   resultsApiUrl: '/ResultsApiDev',
-  webAppApiUrl: 'http://ldms.nordex-ag.com/NET6/JobManagerAPI/api/v1/JobManager/Node/',
+  webAppApiUrl:
+    'http://ldms.nordex-ag.com/NET6/JobManagerAPI/api/v1/JobManager/Node/',
+  userLoginUrl: 'http://ldms.nordex-ag.com/NET6/JobManagerAPI/api/v1/Login',
   navigationsUrl: 'http://ldms/navigations.json',
 };
