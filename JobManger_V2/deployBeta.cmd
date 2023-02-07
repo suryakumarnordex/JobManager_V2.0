b10b3f827f00d984d07c92aa5db40338a1060c98
SrivigneshUmasankaran <UmasankarS@NORDEX-AG.COM>
2023-02-07 13:54:18 +0530

    edited

--- JobManger_V2/deployBeta.cmd
+++ JobManger_V2/deployBeta.cmd
@@ -4,7 +4,7 @@ cd /d %~dp0
 
 REM call npm run build-prod-JobManagerBeta
 REM call ng build --prod --base-href /JobManagerAPIBeta/ --build-optimizer=false
-call ng build --prod --base-href /JobManagerBeta/
+call ng build --base-href /JobManagerBeta/
 
 copy src\web.config dist
 xcopy dist\JobManagerUI\* \\kghapp83v\c$\inetpub\wwwroot\JobManagerBeta /s /e /y
