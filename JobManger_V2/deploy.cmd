a878436ccee01d54517b45f79958c60e76bc559e
SrivigneshUmasankaran <UmasankarS@NORDEX-AG.COM>
2023-03-30 11:16:07 +0530

    deploy command updated

--- JobManger_V2/deploy.cmd
+++ JobManger_V2/deploy.cmd
@@ -2,10 +2,10 @@ set Path=%APPDATA%\npm\;D:\Program Files\nodejs\;%Path%
 
 cd /d %~dp0
 
-REM call npm run build-prod-quickerview
-REM call ng build --prod --base-href /QuickerView_Beta/ --build-optimizer=false
-call ng build --prod --base-href /JobManager/
+REM call npm run build-prod-JobManager
+REM call ng build --prod --base-href /JobManager/ --build-optimizer=false
+call ng build --base-href /JobManager/
 
 copy src\web.config dist
-xcopy dist\JobManagerUI\* \\kghapp83v\c$\inetpub\wwwroot\JobManager /s /e /y
+xcopy dist\JobManagerUI\* \\kghapp83v\C$\inetpub\wwwroot\JobManager /s /e /y
 pause
--- JobManger_V2/deployBeta.cmd
+++ JobManger_V2/deployBeta.cmd
@@ -2,9 +2,9 @@ set Path=%APPDATA%\npm\;D:\Program Files\nodejs\;%Path%
 
 cd /d %~dp0
 
-REM call npm run build-prod-JobManagerBeta
-REM call ng build --prod --base-href /JobManagerAPIBeta/ --build-optimizer=false
-call ng build --base-href /JobManagerBeta/
+REM call npm run build-prod-JobManager
+REM call ng build --prod --base-href /JobManagerBeta/ --build-optimizer=false
+call ng build --base-href /JobManager/
 
 copy src\web.config dist
 xcopy dist\job-manger-v2\* \\kghapp83v\c$\inetpub\wwwroot\JobManagerBeta /s /e /y
