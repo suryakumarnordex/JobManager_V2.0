cf6b5fd7bf33c82f3c0bad7e7d547c35ff8895bf
SrivigneshUmasankaran <UmasankarS@NORDEX-AG.COM>
2023-05-10 17:16:46 +0530

    Localhost commanded

--- JobManger_V2/src/environments/environment.beta.ts
+++ JobManger_V2/src/environments/environment.beta.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  //'https://localhost:57321/api/v1/JobManager/Node/',
   webAppApiUrl:
     'http://ldms.nordex-ag.com/JobManagerAPI/api/v1/JobManager/Node/',
   userLoginUrl:
--- JobManger_V2/src/environments/environment.prod.ts
+++ JobManger_V2/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   resultsApiUrl: '/ResultsApiDev',
+  //'https://localhost:57321/api/v1/JobManager/Node/',
   webAppApiUrl:
     'http://ldms.nordex-ag.com/NET6/JobManagerAPI/api/v1/JobManager/Node/',
   userLoginUrl: 'http://ldms.nordex-ag.com/JobManagerAPI/api/v1/Login',
